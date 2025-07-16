522d216b6df4a37901308db33bd305e011181cbd
sony chaudhary <chaudharysony336@gmail.com>
2025-07-16 18:09:11 +0545

    Commit

--- php/fetch_student.php
+++ php/fetch_student.php
@@ -1,6 +1,6 @@
 <?php
 
-include '../config.php';
+include 'config.php';
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
--- php/signup_login.php
+++ php/signup_login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include '../config.php';
+include 'config.php';
 
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
